900a80f7b3bff12cdebdb83556fbacb33a5ceb3f
sebastian.schmidt <bastman77@gmail.com>
2018-01-09 08:50:18 +0100

    initial

--- rest-api/src/main/kotlin/com/example/api/bookstore/BookStoreApiController.kt
+++ rest-api/src/main/kotlin/com/example/api/bookstore/BookStoreApiController.kt
@@ -1,7 +1,41 @@
 package com.example.api.bookstore
 
-import org.springframework.web.bind.annotation.RestController
+import com.example.api.bookstore.domain.repo.AuthorRepoService
+import mu.KLogging
+import org.springframework.web.bind.annotation.*
+import java.time.Instant
+import java.util.*
 
 @RestController
-class BookStoreApiController {
+class BookStoreApiController(private val authorRepo: AuthorRepoService) {
+
+    @GetMapping("/api/bookstore/author")
+    fun authorsFindAll() =
+            authorRepo.findAll()
+                    .results
+                    .toList()
+                    .map { it.toAuthorDto() }
+
+    @GetMapping("/api/bookstore/author/{id}")
+    fun authorsGetOne(@PathVariable id: UUID) =
+            authorRepo.requireOneById(id)
+                    .toAuthorDto()
+
+    @PutMapping("/api/bookstore/author")
+    fun authorsCreateOne(@RequestBody req: AuthorCreateRequest) =
+            req.toAuthorRecord()
+                    .let { authorRepo.insert(it) }
+                    .also { logger.info { "Updated Record: $it" } }
+                    .toAuthorDto()
+
+    @PostMapping("/api/bookstore/author/{id}")
+    fun authorsUpdateOne(@PathVariable id: UUID, @RequestBody req: AuthorUpdateRequest): AuthorDto
+            = authorRepo.requireOneById(id)
+            .copy(modifiedAt = Instant.now(), name = req.name)
+            .let { authorRepo.update(it) }
+            .also { logger.info { "Updated Record: $it" } }
+            .toAuthorDto()
+
+
+    companion object : KLogging()
 }
---
+++ rest-api/src/main/kotlin/com/example/api/bookstore/apiModel.kt
@@ -0,0 +1,36 @@
+package com.example.api.bookstore
+
+import com.example.api.bookstore.domain.db.Author
+import com.example.api.bookstore.domain.db.BookStatus
+import java.math.BigDecimal
+import java.time.Instant
+import java.util.*
+
+data class AuthorCreateRequest(val name: String)
+data class AuthorUpdateRequest(val name: String)
+data class BookCreateRequest(val authorId: UUID, val title: String, val status: BookStatus, val price: BigDecimal)
+data class BookUpdateRequest(val title: String, val status: BookStatus, val price: BigDecimal)
+
+data class AuthorDto(val id: UUID, val createdAt: Instant, val modifiedAt: Instant, val name: String)
+data class BookDto(
+        val id: UUID,
+        val createdAt: Instant,
+        val modifiedAt: Instant,
+        val title: String,
+        val status: BookStatus,
+        val price: BigDecimal,
+        val author: AuthorDto
+)
+
+fun Author.toAuthorDto() = AuthorDto(id=id,createdAt = createdAt,modifiedAt = modifiedAt,name = name)
+
+fun AuthorCreateRequest.toAuthorRecord():Author {
+    val now=Instant.now()
+    return Author(
+            id = UUID.randomUUID(),
+            createdAt = now,
+            modifiedAt = now,
+            version = 0,
+            name = name
+    )
+}
